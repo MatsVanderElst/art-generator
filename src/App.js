2e58c484f7f0ba84c3257d67f203d4b3b22778c6
mats Van der Elst <matsvanderelst@matss-MacBook-Air.local>
2022-03-03 20:06:34 +0100

    added SVGCircle component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
-const SVGComponent = ({width = 200, height = 200, fill = "rgb(0,0,255)", stroke = "rgb(0,0,0)", strokeWidth = 10 }) => {
+const SVGRect = ({width = 200, height = 200, fill = "rgb(0,0,255)", stroke = "rgb(0,0,0)", strokeWidth = 10 }) => {
   return (
     <svg width={width} height={height}>
       <rect width={width} height={height} style={{fill:fill,stroke:stroke,strokeWidth:strokeWidth}} />
+    </svg>
+    );
+}
+
+const SVGCircle = ({height = 200, width = 200, cx=1/2*width, cy=1/2*width, r=1/2*width, stroke = "rgb(0,0,0)", strokeWidth = 10, fill = "rgb(0,0,255)"   }) => {
+  return (
+    <svg height={height} width={width}>
+      <circle cx={cx} cy={cy} r={r} stroke={stroke} strokeWidth={strokeWidth} fill={fill} />
     </svg>
     );
 }
@@ -13,9 +21,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <SVGComponent/>
-        <SVGComponent width={180}  fill="rgb(0,255,0)"/>
-        <SVGComponent width={200}  fill="rgb(255,0,0)"/>
+        <SVGRect />
+        <SVGRect width={180}  fill="rgb(0,255,0)"/>
+        <SVGRect width={200} fill="rgb(255,0,0)" />
+        <SVGCircle />
       </header>
     </div>
   );
